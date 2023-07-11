2a522918379285482589d9467c11146c6d889775
Alan Jones <alanmjones12@gmail.com>
2023-07-12 00:05:54 +0100

    add Fluent Migrations

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,8 +1,18 @@
+using FluentMigrator.Runner;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddFluentMigratorCore()
+    .ConfigureRunner(config => 
+        config.AddSqlServer()
+        .WithGlobalConnectionString("Server=localhost;Database=master;Trusted_Connection=True;")
+        .ScanIn(Assembly.GetExecutingAssembly()).For.All())
+    .AddLogging(config => config.AddFluentMigratorConsole());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,5 +31,9 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var migrator = scope.ServiceProvider.GetService<IMigrationRunner>();
+migrator.ListMigrations();
 
 app.Run();
