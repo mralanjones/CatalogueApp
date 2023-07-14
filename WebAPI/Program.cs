a863ff22f32bc66240c23bf680d52a3a532ad30e
Alan Jones <alanmjones12@gmail.com>
2023-07-14 22:50:15 +0100

    remove fluent migrator

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,17 +1,8 @@
-using FluentMigrator.Runner;
-using System.Reflection;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddFluentMigratorCore()
-    .ConfigureRunner(config => 
-        config.AddSqlServer()
-        .WithGlobalConnectionString("Server=localhost;Database=master;Trusted_Connection=True;")
-        .ScanIn(Assembly.GetExecutingAssembly()).For.All())
-    .AddLogging(config => config.AddFluentMigratorConsole());
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -31,9 +22,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
-using var scope = app.Services.CreateScope();
-var migrator = scope.ServiceProvider.GetService<IMigrationRunner>();
-migrator.MigrateUp();
 
 app.Run();
